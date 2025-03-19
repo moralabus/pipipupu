cf599b6ddc3d2bcbb996e8dfe34d85cf2f382385
moralabus <100228186+moralabus@users.noreply.github.com>
2025-03-19 23:51:41 +0300

    Update IFlightOperations.cs

--- IFlightOperations.cs
+++ IFlightOperations.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace Курсач
 {
-   
+    // Интерфейс IFlightOperations, определяющий операции, связанные с полетами
     public interface IFlightOperations
     {
+        // Метод для получения возраста объекта, реализующего этот интерфейс
         int GetAge();
     }
-
 }
